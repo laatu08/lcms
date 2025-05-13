b0dc199254c0ed596bdaf64806e4b190ed9d2d63
laatu08 <partha2002borah@gmail.com>
2025-05-13 20:31:35 +0530

    production bug 2

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,6 +6,7 @@ export const generateToken = (res, user, message) => {
     return res.status(200).cookie("token",token,{
         httpOnly:true,
         // sameSite:"strict",
+        secure:true,
         sameSite:"none", // for deployment
         maxAge:24*60*60*1000,
     }).json({success:true,message,user})
